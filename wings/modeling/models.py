60b6c763aca47b681939c27ee990deb60d9e4617
Mateusz Krajewski <mkrajew3@gmail.com>
2025-03-23 22:33:43 +0100

    trained transformer vision

--- wings/modeling/models.py
+++ wings/modeling/models.py
@@ -14,3 +14,18 @@ class ResnetPreTrained(nn.Module):
     def forward(self, x):
         x = self.pretrained_model(x)
         return x
+
+
+class TransformerPreTrained(nn.Module):
+    def __init__(self, pretrained_model, pretrained_weights):
+        super(TransformerPreTrained, self).__init__()
+        self.pretrained_model = pretrained_model(weights=pretrained_weights)
+        for param in self.pretrained_model.parameters():
+            param.requires_grad = False
+        self.linear = nn.Linear(self.pretrained_model.heads.head.in_features, 38)
+        self.pretrained_model.heads.head = self.linear
+
+
+    def forward(self, x):
+        x = self.pretrained_model(x)
+        return x
