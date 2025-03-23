dc60cffa2f4b55599f2ac3b7284e08f9885fd1a1
Mateusz Krajewski <mkrajew3@gmail.com>
2025-03-23 15:27:04 +0100

    lightning training module

---
+++ wings/modeling/litnet.py
@@ -0,0 +1,45 @@
+import lightning as L
+import torch
+import torch.nn as nn
+import torchmetrics
+
+
+class LitNet(L.LightningModule):
+    def __init__(self, model, num_epochs):
+        super().__init__()
+        self.model = model
+        self.criterion = nn.MSELoss()
+        self.num_epochs = num_epochs
+
+        self.mse_test = torchmetrics.regression.MeanSquaredError()
+
+    def training_step(self, batch, batch_idx):
+        x, target = batch
+        target = target.float()
+        output = self.model(x)
+        loss = self.criterion(output, target)
+        self.log('train_loss', loss, on_step=False, on_epoch=True, prog_bar=True)
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, target = batch
+        target = target.float()
+        output = self.model(x)
+        loss = self.criterion(output, target)
+        self.log('val_loss', loss, on_step=False, on_epoch=True, prog_bar=True)
+
+    def test_step(self, batch, batch_idx):
+        x, target = batch
+        target = target.float()
+        output = self.model(x)
+
+        self.mse_test(output, target)
+        self.log('test_loss', self.mse_test, on_step=False, on_epoch=True, prog_bar=True)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.AdamW(self.model.parameters(), lr=1e-3, weight_decay=1e-5)
+        lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=self.num_epochs, eta_min=1e-6)
+        return {"optimizer": optimizer, "lr_scheduler": lr_scheduler}
+
+
