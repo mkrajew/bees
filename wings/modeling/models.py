6d82bbf68dde20f5c1057c6e204f3c082d59b503
Mateusz Krajewski <mkrajew3@gmail.com>
2025-03-23 15:26:03 +0100

    resnet pretrained model

---
+++ wings/modeling/models.py
@@ -0,0 +1,16 @@
+import torch.nn as nn
+
+
+class ResnetPreTrained(nn.Module):
+    def __init__(self, pretrained_model, pretrained_weights):
+        super(ResnetPreTrained, self).__init__()
+
+        self.pretrained_model = pretrained_model(weights=pretrained_weights)
+        for param in self.pretrained_model.parameters():
+            param.requires_grad = False
+        self.linear = nn.Linear(self.pretrained_model.fc.in_features, 38)
+        self.pretrained_model.fc = self.linear
+
+    def forward(self, x):
+        x = self.pretrained_model(x)
+        return x
