5199b021f05779f44d05be93da3927b56d526c31
mkrajew <mkrajew3@gmail.com>
2026-05-09 19:50:13 +0200

    pretrained unet weights

--- wings/modeling/training/custom_unet_training.py
+++ wings/modeling/training/custom_unet_training.py
@@ -5,18 +5,18 @@ Training UNET with masks with square size 5.
 import torch
 from loguru import logger
 
-from wings.config import DEVICE, TRAINING_DIR, PROCESSED_DATA_DIR
+from wings.config import DEVICE, TRAINING_DIR, PROCESSED_DATA_DIR, MODELS_DIR
 from wings.dataset import load_datasets
-from wings.modeling.loss import DiceLoss
+from wings.modeling.loss import DiceLoss, WeightedDiceLoss
 from wings.modeling.train import train
 from wings.dataset import MaskRectangleDataset
 from wings.modeling.unet import UNet
 
-run_num = 2
-run_name = "unet-training"
-model_name = "custom-unet"
+run_num = 1
+run_name = "custom-unet-training"
+model_name = "custom-unet-pretrained"
 PARAMETERS = {
-    "project_name": "bees-wings-modeling3",
+    "project_name": "wingai",
     "logger_save_dir": TRAINING_DIR,
     "run_name": f"{run_name}_{run_num}",
     "checkpoint_save_dir": TRAINING_DIR / "lightning-checkpoints" / model_name,
@@ -28,7 +28,7 @@ PARAMETERS = {
     "num_workers": 8,
     "early_stop_min_delta": 0.1,
     "early_stop_patience": 50,
-    "criterion": DiceLoss(),
+    "criterion": WeightedDiceLoss(landmark_weight=50.0, background_weight=1.0),
 }
 
 if __name__ == "__main__":
@@ -43,6 +43,14 @@ if __name__ == "__main__":
     logger.info("Loaded datasets.")
 
     model = UNet(in_channels=1, out_channels=1, kernel_size=3)
+
+    checkpoint = torch.load(MODELS_DIR / "unet-pytorch.pt")
+    state_dict = checkpoint
+    old_weight = state_dict["encoder1.enc1conv1.weight"]
+    new_weight = old_weight.mean(dim=1, keepdim=True)
+    state_dict["encoder1.enc1conv1.weight"] = new_weight
+    model.load_state_dict(state_dict)
+
     model.to(DEVICE)
 
     train(model, train_val_test_datasets, PARAMETERS)
